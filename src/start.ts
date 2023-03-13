4c5bf95840484a5d498512f0aa9d0b89f49acaaf
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2023-03-13 02:02:42 -0300

    fix: descloustering for deployment

--- src/start.ts
+++ src/start.ts
@@ -1,28 +1,9 @@
 import "reflect-metadata";
-import cluster from "cluster";
 import process from 'node:process';
-import os from "os";
 import { App } from './app';
 require('dotenv').config()
 
-const totalCPUs = os.cpus().length;
-
-if (cluster.isMaster) {
-  console.log(`Number of CPUs is ${totalCPUs}`);
-  console.log(`Master ${process.pid} is running`);
- 
-  for (let i = 0; i < (totalCPUs - 1); i++) {
-    cluster.fork();
-  }
- 
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-    console.log("Let's fork another worker. Burn Baby!");
-    cluster.fork();
-  });
-} else {
-  new App().start().catch(handleError);
-}
+new App().start().catch(handleError);
 
 process.on('uncaughtException', (err: any) => {
   console.log('[UncaughtException]: ', err);
