f1e857cfc9af21168504697d4cf7d311db24fc8c
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2023-03-13 00:39:30 -0300

    feat: add clustering

--- src/start.ts
+++ src/start.ts
@@ -1,14 +1,30 @@
 import "reflect-metadata";
+import cluster from "cluster";
+import process from 'node:process';
+import os from "os";
 import { App } from './app';
 require('dotenv').config()
 
-try {
+const totalCPUs = os.cpus().length;
+
+if (cluster.isPrimary) {
+  console.log(`Number of CPUs is ${totalCPUs}`);
+  console.log(`Master ${process.pid} is running`);
+ 
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
+ 
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker. Burn Baby!");
+    cluster.fork();
+  });
+} else {
   new App().start().catch(handleError);
-} catch (e) {
-  handleError(e);
 }
 
-process.on('uncaughtException', err => {
+process.on('uncaughtException', (err: any) => {
   console.log('[UncaughtException]: ', err);
   process.exit(1);
 });
